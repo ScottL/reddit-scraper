e2b088bbb589e1822768f79f53f8c37df8b92484
Scott Lin <11ScottL5@gmail.com>
2014-02-25 02:22:43 -0800

    initial code commit. very messy. not working

---
+++ test.py
@@ -0,0 +1,54 @@
+import praw
+from collections import deque
+from time import sleep
+from operator import itemgetter
+import json
+import os
+
+b = os.path.getsize("out.txt")
+useragent = "corpusbuilder/1.0 by scott"
+cache = deque(maxlen = 350)
+
+r = praw.Reddit(user_agent=useragent)
+running = True
+
+s = set([])
+if b == 0:
+   dict = {}
+else:
+   dict= json.load(open("out.txt"))
+   dict = [s.encode('utf-8') for s,t in dict]
+   print dict
+punc = ['?','!',',','.',':','"',';','(',')','[',']']
+
+while running:
+   allcomments = r.get_comments('all', limit = 340)
+   print "retrieved comments"
+   for x in allcomments:
+      if x.id in cache:
+         print "IN CACHE"
+         print "Sleeping"
+         sleep(30)
+         break
+      cache.append(x.id)
+
+      try:
+         for i in x.body.split():
+            j = i.encode('utf-8').strip()
+            print j
+            if j in dict:
+               print "already in"
+               dict[j] += 1
+            else:
+               print "adding"
+               dict[j] = 1
+      except Exception, e:
+         print "ERROR: ", e
+         print "Sleeping"
+         sleep(30)
+
+   
+   sortD = sorted(dict.iteritems(), key=itemgetter(1), reverse=True)
+   print sortD
+   open('out.txt','w').close()
+   json.dump(sortD, open("out.txt",'w'))
