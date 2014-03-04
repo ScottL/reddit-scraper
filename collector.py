d133fbe9d83a5f065873874299ca313363acf7bb
Scott Lin <11ScottL5@gmail.com>
2014-03-03 23:25:09 -0800

    Don't use JSON load

--- collector.py
+++ collector.py
@@ -5,50 +5,62 @@ from operator import itemgetter
 import json
 import os
 
-b = os.path.getsize("out.txt")
-useragent = "corpusbuilder/1.0 by scott"
-cache = deque(maxlen = 350)
 
-r = praw.Reddit(user_agent=useragent)
-running = True
+def extract():
+   b = os.path.getsize("out.txt")
+   useragent = "corpusbuilder/1.0 by scott"
+   cache = deque(maxlen = 350)
 
-s = set([])
-if b == 0:
+   r = praw.Reddit(user_agent=useragent)
+   running = True
+
    dict = {}
-else:
-   dict= json.load(open("out.txt"))
-   dict = [s.encode('utf-8') for s,t in dict]
-   print dict
-punc = ['?','!',',','.',':','"',';','(',')','[',']']
+   if b == 0:
+      #dict = {}
+      print "t"
+   else:
+      #dict= json.load(open("out.txt"))
+      #dict = [s.encode('utf-8') for s,t in dict]
+      print "t"
+   punc = ['?','!',',','.',':','"',';','(',')','[',']']
 
-while running:
-   allcomments = r.get_comments('all', limit = 340)
-   print "retrieved comments"
-   for x in allcomments:
-      if x.id in cache:
-         print "IN CACHE"
-         print "Sleeping"
-         sleep(30)
-         break
-      cache.append(x.id)
+   while running:
+      allcomments = r.get_comments('all', limit = 340)
+      print "retrieved comments"
+      for x in allcomments:
+         if x.id in cache:
+            print "IN CACHE"
+            print "Sleeping"
+            sleep(30)
+            break
+         cache.append(x.id)
 
-      try:
-         for i in x.body.split():
-            j = i.encode('utf-8').strip()
-            #print j
-            if j in dict:
-               print "already in"
-               dict[j] += 1
-            else:
-               print "adding"
-               dict[j] = 1
-      except Exception, e:
-         print "ERROR: ", e
-         print "Sleeping"
-         sleep(30)
+         try:
+            for i in x.body.split():
+               j = i.encode('utf-8').strip()
+               #print j
+               if j in dict:
+                  print "already in"
+                  dict[j] += 1
+               else:
+                  print "adding"
+                  dict[j] = 1
+         except Exception, e:
+            print "ERROR: ", e
+            print "Sleeping"
+            sleep(30)
 
-   
-   sortD = sorted(dict.iteritems(), key=itemgetter(1), reverse=True)
-   print sortD
-   open('out.txt','w').close()
-   json.dump(sortD, open("out.txt",'w'))
+      
+      sortD = sorted(dict.iteritems(), key=itemgetter(1), reverse=True)
+      print sortD
+      open('out.txt','w').close()
+      json.dump(sortD, open("out.txt",'w'))
+
+
+def formateprint(dictionary):
+   print "temp"
+
+
+
+
+extract()
--- prettyprint.py
+++
@@ -1,7 +0,0 @@
-# corpus output should be JSON
-# for viewing, should print out corpus with formatting
-
-def prettyprint():
-   print "temp"
-
-
